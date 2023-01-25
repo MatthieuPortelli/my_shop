0ad819c130ff177e5d3959c4666ddc2af8f86dc5
Matthieu <matthieu.portelli@gmail.com>
2023-01-25 12:36:40 +0100

    modify email confirm

--- myshop/info.py
+++ myshop/info.py
@@ -2,9 +2,9 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # Utilisation de GMAIL
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'matthieu.portelli@gmail.com'
-EMAIL_HOST_USER = 'matthieu.portelli@gmail.com'
-EMAIL_HOST_PASSWORD = 'krdioyckvjcgopuz'
+EMAIL_FROM = 'mywebsite.python@gmail.com'
+EMAIL_HOST_USER = 'mywebsite.python@gmail.com'
+EMAIL_HOST_PASSWORD = 'czlionvnaedvtbbm'
 EMAIL_PORT = 587
 # Indique si une connexion TLS (sécurisée) doit être utilisée pour le dialogue avec le serveur SMTP
 EMAIL_USE_TLS = True
